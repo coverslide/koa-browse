c139d9f08808b8f46bce49f5cd1d02eae16d1bc8
Richard Hoffman <richard.j.hoffman@gmail.com>
2017-03-07 18:13:19 -0800

    init

--- index.js
+++ index.js
@@ -1,14 +1,112 @@
-const koa = require('koa');
+const fs = require('mz/fs');
+const { join, extname } = require('path');
 
-const app = koa();
-
-
-const middleware = function (options) {
+exports = module.exports = function (options = {}) {
   const root = options.root;
   return async function (ctx, next) {
+    ctx.browse = {};
+    const parts = ctx.request.path.split('/');
+    if (options.blacklist) {
+      const blacklisted = parts.some(part => options.blacklist.includes(part));
+      if (blacklisted) {
+        return await next();
+      }
+    }
+    ctx.browse.path = join(root, ctx.request.path);
+    try {
+      const stat = await fs.stat(ctx.browse.path);
+      ctx.browse.stat = await fs.stat(ctx.browse.path);
+      if (options.showStat) {
+        ctx.browse.result = ctx.browse.stat;
+      }
+    } catch (e) {
+      ctx.browse.error = 'Could not read file';
+    }
+    await next();
   };
 };
 
-app.use(middleware({root: __dirname}));
+exports.dirList = function (options = {}) {
+  const blacklist = options.blacklist && options.blacklist.map(filename => filename.toLowerCase());
+  const extensionsWhitelist = options.extensionsWhitelist && options.extensionsWhitelist.map(ext => ext.replace(/^\./, '').toLowerCase());
+  const extensionsBlacklist = options.extensionsBlacklist && options.extensionsBlacklist.map(ext => ext.replace(/^\./, '').toLowerCase());
+  return async function (ctx, next) {
+    if (!ctx.browse || ctx.browse.error || ctx.browse.result || !ctx.browse.path || !ctx.browse.stat || !ctx.browse.stat.isDirectory()) {
+      return await next();
+    }
+    const files = await fs.readdir(ctx.browse.path);
+    ctx.browse.result = await Promise.all(
+      files.map(
+        async (filename) => {
+          const stat = await fs.stat(join(ctx.browse.path, filename));
+          return Object.assign({ directory: stat.isDirectory(), filename }, stat);
+        }
+      )
+    );
+    if (options.hideDotFiles) {
+      ctx.browse.result = ctx.browse.result.filter(file => !file.filename.match(/^\./));
+    }
+    if (blacklist) {
+      ctx.browse.result = ctx.browse.result.filter(file => !blacklist.includes(file.filename.toLowerCase()));
+    }
+    if (extensionsWhitelist) {
+      ctx.browse.result = ctx.browse.result.filter(file => file.directory || extensionsWhitelist.includes(extname(file.filename).replace(/^\./, '').toLowerCase()));
+    }
+    if (extensionsBlacklist) {
+      ctx.browse.result = ctx.browse.result.filter(file => file.directory || !extensionsBlacklist.includes(extname(file.filename).replace(/^\./, '').toLowerCase()));
+    }
+    if (options.sort) {
+      ctx.browse.result.sort((a, b) => {
+        if (a.directory && !b.directory) {
+          return -1;
+        } else if (!a.directory && b.directory) {
+          return 1;
+        } else {
+          const a2 = a.filename.toLowerCase();
+          const b2 = b.filename.toLowerCase();
+          return a2 > b2 ? 1 : a2 < b2 ? -1 : 0;
+        }
+      });
+    }
+    await next();
+  };
+};
 
-app.listen(3020);
+exports.sendFile = function (options = {}) {
+  const extensionsWhitelist = options.extensionsWhitelist && options.extensionsWhitelist.map(ext => ext.replace(/^\./, '').toLowerCase());
+  const extensionsBlacklist = options.extensionsBlacklist && options.extensionsBlacklist.map(ext => ext.replace(/^\./, '').toLowerCase());
+  return async function (ctx, next) {
+    if (!ctx.browse || ctx.browse.error || ctx.browse.result || !ctx.browse.path || !ctx.browse.stat || !ctx.browse.stat.isFile()) {
+      return await next();
+    }
+    if (extensionsWhitelist) {
+      if (!extensionsWhitelist.includes(extname(ctx.browse.path).replace(/^\./, '').toLowerCase())) {
+        return await next();
+      }
+    }
+    if (extensionsBlacklist) {
+      if (extensionsBlacklist.includes(extname(ctx.browse.path).replace(/^\./, '').toLowerCase())) {
+        return await next();
+      }
+    }
+    ctx.response.type = extname(ctx.browse.path);
+    ctx.browse.result = fs.createReadStream(ctx.browse.path);
+    await next();
+  }
+}
+
+exports.result = function () {
+  return async function (ctx, next) {
+    if (!ctx.browse) {
+      return await next();
+    }
+    if (ctx.browse.error) {
+      ctx.response.body = { error: ctx.browse.error };
+    } else if (ctx.browse.result) {
+      ctx.response.body = ctx.browse.result;
+    }
+    await next();
+  };
+};
+
+exports.browse = exports;
