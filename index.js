35a43fb3506ac4f5435cdb4faef3e3595da6061b
Richard Hoffman <richard.j.hoffman@gmail.com>
2017-03-09 16:36:10 -0800

    Added ESLint

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const fs = require('mz/fs');
 const { join, extname } = require('path');
+const naturalCompare = require('string-natural-compare');
 
-exports = module.exports = function (options = {}) {
+module.exports = function browse(options = {}) {
   const root = options.root;
-  return async function (ctx, next) {
+  return async function browseAsync(ctx, next) {
     ctx.browse = {};
     const parts = ctx.request.path.split('/');
     if (options.blacklist) {
       const blacklisted = parts.some(part => options.blacklist.includes(part));
       if (blacklisted) {
-        return await next();
+        await next();
+        return;
       }
     }
     ctx.browse.path = join(root, ctx.request.path);
     try {
-      const stat = await fs.stat(ctx.browse.path);
       ctx.browse.stat = await fs.stat(ctx.browse.path);
-      if (options.showStat) {
-        ctx.browse.result = ctx.browse.stat;
-      }
     } catch (e) {
       ctx.browse.error = 'Could not read file';
     }
@@ -26,13 +24,23 @@ exports = module.exports = function (options = {}) {
   };
 };
 
-exports.dirList = function (options = {}) {
+exports = module.exports;
+
+exports.dirList = function dirList(options = {}) {
   const blacklist = options.blacklist && options.blacklist.map(filename => filename.toLowerCase());
   const extensionsWhitelist = options.extensionsWhitelist && options.extensionsWhitelist.map(ext => ext.replace(/^\./, '').toLowerCase());
   const extensionsBlacklist = options.extensionsBlacklist && options.extensionsBlacklist.map(ext => ext.replace(/^\./, '').toLowerCase());
-  return async function (ctx, next) {
-    if (!ctx.browse || ctx.browse.error || ctx.browse.result || !ctx.browse.path || !ctx.browse.stat || !ctx.browse.stat.isDirectory()) {
-      return await next();
+  return async function dirListAsync(ctx, next) {
+    if (
+      !ctx.browse ||
+      ctx.browse.error ||
+      ctx.browse.result ||
+      !ctx.browse.path ||
+      !ctx.browse.stat ||
+      !ctx.browse.stat.isDirectory()
+    ) {
+      await next();
+      return;
     }
     const files = await fs.readdir(ctx.browse.path);
     ctx.browse.result = await Promise.all(
@@ -40,14 +48,16 @@ exports.dirList = function (options = {}) {
         async (filename) => {
           const stat = await fs.stat(join(ctx.browse.path, filename));
           return Object.assign({ directory: stat.isDirectory(), filename }, stat);
-        }
-      )
+        },
+      ),
     );
     if (options.hideDotFiles) {
       ctx.browse.result = ctx.browse.result.filter(file => !file.filename.match(/^\./));
     }
     if (blacklist) {
-      ctx.browse.result = ctx.browse.result.filter(file => !blacklist.includes(file.filename.toLowerCase()));
+      ctx.browse.result = ctx.browse.result.filter(file =>
+        !blacklist.includes(file.filename.toLowerCase()),
+      );
     }
     if (extensionsWhitelist) {
       ctx.browse.result = ctx.browse.result.filter(file => file.directory || extensionsWhitelist.includes(extname(file.filename).replace(/^\./, '').toLowerCase()));
@@ -61,44 +71,65 @@ exports.dirList = function (options = {}) {
           return -1;
         } else if (!a.directory && b.directory) {
           return 1;
-        } else {
-          const a2 = a.filename.toLowerCase();
-          const b2 = b.filename.toLowerCase();
-          return a2 > b2 ? 1 : a2 < b2 ? -1 : 0;
         }
+        return naturalCompare(a.filename.toLowerCase(), b.filename.toLowerCase());
       });
     }
     await next();
   };
 };
 
-exports.sendFile = function (options = {}) {
+exports.sendFile = function sendFile(options = {}) {
   const extensionsWhitelist = options.extensionsWhitelist && options.extensionsWhitelist.map(ext => ext.replace(/^\./, '').toLowerCase());
   const extensionsBlacklist = options.extensionsBlacklist && options.extensionsBlacklist.map(ext => ext.replace(/^\./, '').toLowerCase());
-  return async function (ctx, next) {
-    if (!ctx.browse || ctx.browse.error || ctx.browse.result || !ctx.browse.path || !ctx.browse.stat || !ctx.browse.stat.isFile()) {
-      return await next();
+  return async function sendFileAsync(ctx, next) {
+    if (
+      !ctx.browse ||
+      ctx.browse.error ||
+      ctx.browse.result ||
+      !ctx.browse.path ||
+      !ctx.browse.stat ||
+      !ctx.browse.stat.isFile()
+    ) {
+      await next();
+      return;
     }
     if (extensionsWhitelist) {
       if (!extensionsWhitelist.includes(extname(ctx.browse.path).replace(/^\./, '').toLowerCase())) {
-        return await next();
+        await next();
+        return;
       }
     }
     if (extensionsBlacklist) {
       if (extensionsBlacklist.includes(extname(ctx.browse.path).replace(/^\./, '').toLowerCase())) {
-        return await next();
+        await next();
+        return;
       }
     }
     ctx.response.type = extname(ctx.browse.path);
     ctx.browse.result = fs.createReadStream(ctx.browse.path);
     await next();
-  }
-}
+  };
+};
 
-exports.result = function () {
-  return async function (ctx, next) {
+exports.download = function download() {
+  return async function downloadAsync(ctx, next) {
+    if (!ctx.browse || ctx.browse.error || !ctx.browse.result) {
+      await next();
+      return;
+    }
+    if (ctx.query.download === 'true') {
+      ctx.response.attachment();
+    }
+    await next();
+  };
+};
+
+exports.result = function result() {
+  return async function resultAsync(ctx, next) {
     if (!ctx.browse) {
-      return await next();
+      await next();
+      return;
     }
     if (ctx.browse.error) {
       ctx.response.body = { error: ctx.browse.error };
